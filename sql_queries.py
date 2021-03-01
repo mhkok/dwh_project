701173db8f0e3d0437f2688df6c5e681c2537797
mhkok <matthijs.kok@shell.com>
2021-03-01 12:01:26 +0100

    add constraints & distinct

--- sql_queries.py
+++ sql_queries.py
@@ -62,15 +62,19 @@ staging_songs_table_create = (
 songplay_table_create = (
     """
     CREATE TABLE songplay (
-        songplay_id INT IDENTITY(0,1), 
+        songplay_id INT IDENTITY(0,1) PRIMARY KEY,
         start_time BIGINT NOT NULL, 
-        user_id INT,
+        user_id INT NOT NULL,
         level VARCHAR(255), 
         song_id VARCHAR(255), 
-        artist_id VARCHAR(255), 
+        artist_id VARCHAR(255) NOT NULL, 
         session_id INT, 
         location VARCHAR(255), 
-        user_agent VARCHAR(255)
+        user_agent VARCHAR(255),
+        FOREIGN KEY (start_time) REFERENCES time (start_time),
+        FOREIGN KEY (user_id) REFERENCES users (user_id),
+        FOREIGN KEY (song_id) REFERENCES songs (song_id),
+        FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
     );
     """
 )
@@ -85,7 +89,7 @@ user_table_create = (
         level VARCHAR(255)
     );
     """
-)   
+)
 
 song_table_create = (
     """
@@ -102,7 +106,7 @@ song_table_create = (
 artist_table_create = (
     """
     CREATE TABLE artists (
-        artist_id VARCHAR(255) NOT NULL PRIMARY KEY, 
+        artist_id VARCHAR(255) PRIMARY KEY, 
         name VARCHAR(255), 
         location VARCHAR(255), 
         latitude FLOAT, 
@@ -150,11 +154,11 @@ staging_events_copy = (
 user_table_insert = (
     """
     INSERT INTO users (user_id, first_name, last_name, gender, level)
-    SELECT se.userid,
-           se.firstname,
-           se.lastname,
-           se.gender,
-           se.level
+    SELECT DISTINCT se.userid,
+                    se.firstname,
+                    se.lastname,
+                    se.gender,
+                    se.level
     FROM staging_events se
     WHERE userid IS NOT NULL
     """
@@ -175,12 +179,13 @@ song_table_insert = (
 artist_table_insert = (
     """
     INSERT INTO artists (artist_id, name, location, latitude, longitude)
-    SELECT  ss.artist_id,
-            ss.artist_name,
-            ss.artist_location,
-            ss.artist_latitude,
-            ss.artist_longitude
+    SELECT  DISTINCT ss.artist_id,
+                     ss.artist_name,
+                     ss.artist_location,
+                     ss.artist_latitude,
+                     ss.artist_longitude
     FROM staging_songs ss
+    WHERE artist_id IS NOT NULL
     """
 )
 
@@ -201,24 +206,24 @@ time_table_insert = (
 songplay_table_insert = (
     """
    INSERT INTO songplay (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-   SELECT   se.ts,
-            se.userid,
-            se.level,
-            ss.song_id,
-            ss.artist_id,
-            se.sessionid,
-            se.location,
-            se.useragent
+   SELECT DISTINCT  se.ts,
+                    se.userid,
+                    se.level,
+                    ss.song_id,
+                    ss.artist_id,
+                    se.sessionid,
+                    se.location,
+                    se.useragent
     FROM staging_events se 
     LEFT JOIN staging_songs ss ON (se.artist = ss.artist_name) 
     AND (se.length = ss.duration)
-    WHERE se.page = 'NextSong';
+    WHERE se.page = 'NextSong' AND artist_id IS NOT NULL;
     """
 )
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
